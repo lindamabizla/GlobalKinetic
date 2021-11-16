ec92c0ad6e208ed0675795daecc7e449163808eb
lindamabizla <94310635+lindamabizla@users.noreply.github.com>
2021-11-16 07:28:25 +0200

    Add files via upload

---
+++ UI/Models/CoinJarHeaderModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI.Models
+{
+    public class CoinJarHeaderModel
+    {
+        public decimal Amount { get; set; }
+        public decimal Volume { get; set; }
+    }
+}
