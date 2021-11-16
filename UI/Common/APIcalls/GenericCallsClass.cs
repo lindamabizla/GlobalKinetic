0f69a482ca65c04a5025603ed4d9af78cfccbc1a
lindamabizla <94310635+lindamabizla@users.noreply.github.com>
2021-11-16 07:27:44 +0200

    Add files via upload

---
+++ UI/Common/APIcalls/GenericCallsClass.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace UI.Common.APIcalls
+{
+    public class GenericCallsClass
+    {
+        public HttpResponseMessage GetAsyncCall(string url, string call)
+        {
+            string BaseAddressAPI = url;
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(BaseAddressAPI)
+            };
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.Timeout = TimeSpan.FromMinutes(30);
+
+            HttpResponseMessage response = client.GetAsync(call).Result;
+
+            return response;
+        }
+
+        public HttpResponseMessage PostAsJsonAsyncCall(string url, string call, Object model)
+        {
+            string BaseAddressAPI = url;
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(BaseAddressAPI)
+            };
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.Timeout = TimeSpan.FromMinutes(30);
+
+            HttpResponseMessage response = client.PostAsJsonAsync(call, model).Result;
+
+            return response;
+        }
+
+        public HttpResponseMessage PutAsJsonAsyncCall(string url, string call, Object model)
+        {
+            string BaseAddressAPI = url;
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(BaseAddressAPI)
+            };
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.Timeout = TimeSpan.FromMinutes(30);
+
+            HttpResponseMessage response = client.PutAsJsonAsync(call, model).Result;
+
+            return response;
+        }
+
+        public HttpResponseMessage DeleteAsJsonAsyncCall(string url, string call)
+        {
+            string BaseAddressAPI = url;
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(BaseAddressAPI)
+            };
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.Timeout = TimeSpan.FromMinutes(30);
+
+            HttpResponseMessage response = client.DeleteAsync(call).Result;
+
+            return response;
+        }
+    }
+}
